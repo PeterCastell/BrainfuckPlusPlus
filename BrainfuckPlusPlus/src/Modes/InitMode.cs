2b03641f55cd87fe1f3e600c1363817117a5b56f
Peter Castell <peter@castell.org>
2026-09-09 13:23:27 -0400

    init path

--- BrainfuckPlusPlus/src/Modes/InitMode.cs
+++ BrainfuckPlusPlus/src/Modes/InitMode.cs
@@ -9,12 +9,21 @@ public class InitMode : Mode
     public void Execute(ReadOnlySpan<string> args)
     {
         var assembly = typeof(ZigTemplater).Assembly;
+        var projectDir = args.Length switch
+        {
+            0 => Directory.GetCurrentDirectory(),
+            1 => Path.GetFullPath(args[0]),
+            _ => throw new ArgumentException("init accepts at most one path")
+        };
+
+        Directory.CreateDirectory(projectDir);
 
         void CreateFile(string fileName)
         {
-            if (File.Exists(fileName)) return;
+            var path = Path.Combine(projectDir, fileName);
+            if (File.Exists(path)) return;
 
-            using var fileStream = File.Create(fileName);
+            using var fileStream = File.Create(path);
             using var resourceStream = assembly.GetManifestResourceStream("BrainfuckPlusPlus.example." + fileName)!;
             resourceStream.CopyTo(fileStream);
         }
